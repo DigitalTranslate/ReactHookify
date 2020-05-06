01960be7d652927567c85d5fa1409ebc4a8ef4d9
Anderson Chan <andersonchan4574@gmail.com>
2020-05-06 14:34:23 -0400

    test

---
+++ client/constructorUtil.js
@@ -0,0 +1,1 @@
+function handleConstructor(str) {}
--- client/test1.js
+++ client/test1.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-lonely-if */
+const fs = require('fs')
+
 let str = `import statement
 
 class    testApp extends Component {
@@ -8,76 +10,70 @@ class    testApp extends Component {
   }
 }
 export something
-`;
+`
 
 function translate(string) {
-  const propsCheck = string.includes('props') ? 'props' : '';
-  const nameOfClass = getClassName(string);
-  const renderSlice = getRender(string);
+  const propsCheck = string.includes('props') ? 'props' : ''
+  const nameOfClass = getClassName(string)
+  const renderSlice = getRender(string)
 
   return `function ${nameOfClass}(${propsCheck}) {
     return (
       ${renderSlice}
     )
-  }`;
+  }`
 }
 
-/*
+//TEST FOR WRITING FILE
+// fs.writeFile('proof.js', translate(str), (err) => {
+//   if (err) throw err
+//   console.log('done')
+// })
 
-expected output:
-
-function testApp() {
-  return (
-    <div>hi</div>
-  )
-}
-
-*/
-
-console.log(translate(str));
+console.log(translate(str))
 
 function getClassName(string) {
-  const classIdx = string.indexOf('class');
-  const classSlice = string.slice(classIdx);
-  const nameOfClass = classSlice.split(/[ ]+/)[1];
-  return nameOfClass;
+  const classIdx = string.indexOf('class')
+  const classSlice = string.slice(classIdx)
+  const nameOfClass = classSlice.split(/[ ]+/)[1]
+  return nameOfClass
 }
 
 function getRender(string) {
-  let renderIdx = string.indexOf('render');
-  renderIdx = string.indexOf('{', renderIdx);
-  let renderEndIdx = renderIdx + 1;
-  let renderSlice = string.slice(renderIdx, renderEndIdx);
+  let renderIdx = string.indexOf('render')
+  renderIdx = string.indexOf('{', renderIdx)
+  let renderEndIdx = renderIdx + 1
+  let renderSlice = string.slice(renderIdx, renderEndIdx)
   while (!validBraces(renderSlice)) {
-    renderEndIdx++;
-    renderSlice = string.slice(renderIdx, renderEndIdx);
+    renderEndIdx++
+    renderSlice = string.slice(renderIdx, renderEndIdx)
   }
   renderSlice = renderSlice
     .slice(renderSlice.indexOf('return') + 6, renderSlice.length - 2)
-    .trim();
-  return renderSlice;
+    .trim()
+  return renderSlice
 }
 
 function validBraces(braces) {
-  let matches = { '(': ')', '{': '}', '[': ']' };
-  let stack = [];
-  let currentChar;
+  let matches = { '(': ')', '{': '}', '[': ']' }
+  let stack = []
+  let currentChar
 
   for (let i = 0; i < braces.length; i++) {
-    currentChar = braces[i];
+    currentChar = braces[i]
 
     if ('(){}[]'.includes(currentChar)) {
       if (matches[currentChar]) {
         // opening braces
-        stack.push(currentChar);
+        stack.push(currentChar)
       } else {
         // closing braces
         if (currentChar !== matches[stack.pop()]) {
-          return false;
+          return false
         }
       }
     }
   }
 
-  return stack.length === 0; // any unclosed braces left?
+  return stack.length === 0 // any unclosed braces left?
 }
