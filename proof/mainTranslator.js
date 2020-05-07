29832b825c2bb36ea390f1b4fbb9093d3487b992
Cameron Ratliff <cratliff@colgate.edu>
2020-05-06 22:08:59 -0400

    fixing getBody function 2

--- proof/mainTranslator.js
+++ proof/mainTranslator.js
@@ -1,6 +1,6 @@
-const { str0, str1, str2, str3 } = require('./utils/exampleClass')
-const { handleConstructor } = require('./utils/constructorUtil')
-const fs = require('fs')
+const { str0, str1, str2, str3 } = require('./utils/exampleClass');
+const { handleConstructor } = require('./utils/constructorUtil');
+const fs = require('fs');
 const {
   getBody,
   getBetween,
@@ -8,28 +8,28 @@ const {
   getInsideOfFunc,
   validBraces,
   getClassName,
-} = require('./utils/commonUtils')
-const { getBeforeReturn } = require('./utils/renderUtil')
+} = require('./utils/commonUtils');
+const { getBeforeReturn } = require('./utils/renderUtil');
 
 //THIS FUNCTION IS IN CHARGE OF TRANSLATING CLASS COMPONENT TO FUNCTIONAL COMPONENT && PUTS IT ALL TOGETHER
 function translateToFunctionComp(classCompInStr) {
-  const nameOfClass = getClassName(classCompInStr)
-  const propsCheck = classCompInStr.includes('props') ? 'props' : ''
+  const nameOfClass = getClassName(classCompInStr);
+  const propsCheck = classCompInStr.includes('props') ? 'props' : '';
 
-  const handledConstructor = handleConstructor(classCompInStr)
-  const constructorEndIdx = getEndIdxOfFunc(classCompInStr, 'constructor')
+  const handledConstructor = handleConstructor(classCompInStr);
+  const constructorEndIdx = getEndIdxOfFunc(classCompInStr, 'constructor');
   const betweenConstructorAndRender = getBetween(
     classCompInStr,
     constructorEndIdx
-  )
-  let funcs = []
-  let pointer = 0
-  getBody(funcs, pointer, betweenConstructorAndRender)
+  );
+  let funcs = [];
+  let pointer = 0;
+  getBody(funcs, pointer, betweenConstructorAndRender);
 
-  const funcCheck = funcs.length ? true : false
+  const funcCheck = funcs.length ? true : false;
 
-  const beforeReturn = getBeforeReturn(classCompInStr)
-  const returnSlice = getInsideOfFunc(classCompInStr, 'render')
+  const beforeReturn = getBeforeReturn(classCompInStr);
+  const returnSlice = getInsideOfFunc(classCompInStr, 'render');
 
   let finalStr = `function ${nameOfClass}(${propsCheck}) {
     ${handledConstructor}
@@ -38,17 +38,17 @@ function translateToFunctionComp(classCompInStr) {
     return (
       ${returnSlice}
     )
-  }`
-  return finalStr.replace(/this.props/gi, 'props')
+  }`;
+  return finalStr.replace(/this.props/gi, 'props');
 }
 
 //THIS FUNCTION TAKES CREATED STRING AND WRITES A FILE
 function createFunctionComponentFile(funcCompInStr) {
   fs.writeFile('proof.js', funcCompInStr, (err) => {
-    if (err) throw err
-    console.log('Made proof.js')
-  })
+    if (err) throw err;
+    console.log('Made proof.js');
+  });
 }
 
-const finalStr = translateToFunctionComp(str0)
-createFunctionComponentFile(finalStr)
+const finalStr = translateToFunctionComp(str0);
+createFunctionComponentFile(finalStr);
--- proof/utils/commonUtils.js
+++ proof/utils/commonUtils.js
@@ -1,73 +1,108 @@
 /* eslint-disable no-lonely-if */
 function getClassName(string) {
-  const classIdx = string.indexOf('class')
-  const classSlice = string.slice(classIdx)
-  const nameOfClass = classSlice.split(/[ ]+/)[1]
-  return nameOfClass
+  const classIdx = string.indexOf('class');
+  const classSlice = string.slice(classIdx);
+  const nameOfClass = classSlice.split(/[ ]+/)[1];
+  return nameOfClass;
 }
 
 function validBraces(braces) {
-  let matches = { '(': ')', '{': '}', '[': ']' }
-  let stack = []
-  let currentChar
+  let matches = { '(': ')', '{': '}', '[': ']' };
+  let stack = [];
+  let currentChar;
   for (let i = 0; i < braces.length; i++) {
-    currentChar = braces[i]
+    currentChar = braces[i];
     if ('(){}[]'.includes(currentChar)) {
       if (matches[currentChar]) {
         // opening braces
-        stack.push(currentChar)
+        stack.push(currentChar);
       } else {
         // closing braces
         if (currentChar !== matches[stack.pop()]) {
-          return false
+          return false;
         }
       }
     }
   }
-  return stack.length === 0 // any unclosed braces left?
+  return stack.length === 0; // any unclosed braces left?
 }
 function getInsideOfFunc(string, methodStr) {
-  let startIdx = string.indexOf(methodStr)
-  startIdx = string.indexOf('{', startIdx)
-  let endIdx = startIdx + 1
-  let funcSlice = string.slice(startIdx, endIdx)
+  // let startIdx = string.indexOf(methodStr);
+  let startIdx = funcNameStartIdx(string, methodStr);
+  startIdx = string.indexOf('{', startIdx);
+  let endIdx = startIdx + 1;
+  let funcSlice = string.slice(startIdx, endIdx);
   while (!validBraces(funcSlice)) {
-    endIdx++
-    funcSlice = string.slice(startIdx, endIdx)
+    endIdx++;
+    funcSlice = string.slice(startIdx, endIdx);
   }
   funcSlice = funcSlice
     .slice(funcSlice.indexOf('return') + 6, funcSlice.length - 2)
-    .trim()
-  return funcSlice
+    .trim();
+  return funcSlice;
 }
 function getEndIdxOfFunc(string, methodStr) {
-  let startIdx = string.indexOf(methodStr)
-  startIdx = string.indexOf('{', startIdx)
-  let endIdx = startIdx + 1
-  let funcSlice = string.slice(startIdx, endIdx)
+  // let startIdx = string.indexOf(methodStr);
+  let startIdx = funcNameStartIdx(string, methodStr);
+  startIdx = string.indexOf('{', startIdx);
+  let endIdx = startIdx + 1;
+  let funcSlice = string.slice(startIdx, endIdx);
   while (!validBraces(funcSlice)) {
-    endIdx++
-    funcSlice = string.slice(startIdx, endIdx)
+    endIdx++;
+    funcSlice = string.slice(startIdx, endIdx);
   }
-  return endIdx
+  return endIdx;
 }
 function getBetween(string, startIdx) {
-  return string.slice(startIdx, string.indexOf('render')).trim()
+  return string.slice(startIdx, string.indexOf('render')).trim();
 }
+
 function getBody(funcs, pointer, bodyStr) {
   while (pointer < bodyStr.length) {
-    let parenIdx = bodyStr.indexOf('(', pointer)
-    let funcStartIdx = pointer
+    let parenIdx = bodyStr.indexOf('(', pointer);
+    let funcStartIdx = pointer;
     while (bodyStr[funcStartIdx] === ' ' || bodyStr[funcStartIdx] === '\n') {
-      funcStartIdx++
+      funcStartIdx++;
     }
-    let funcName = bodyStr.slice(funcStartIdx, parenIdx)
-    let inside = getInsideOfFunc(bodyStr, `${funcName}`)
-    pointer = getEndIdxOfFunc(bodyStr, `${funcName}`)
-    let FuncStringified = `function ${funcName}() {
+    let funcName = bodyStr.slice(funcStartIdx, parenIdx);
+    let inside = getInsideOfFunc(bodyStr, `${funcName}`);
+    pointer = getEndIdxOfFunc(bodyStr, `${funcName}`);
+    let FuncStringified = `function ${funcName.trim()}() {
       ${inside}
-    }`
-    funcs.push(FuncStringified)
+    }`;
+    funcs.push(FuncStringified);
+  }
+}
+
+function funcNameStartIdx(string, methodStr) {
+  console.log(string);
+  console.log(methodStr);
+  let startIdx = string.indexOf(methodStr);
+  let endIdx = minBesidesNegOne(
+    string.indexOf('(', startIdx),
+    string.indexOf(' ', startIdx)
+  );
+  console.log('startIdx', startIdx);
+  console.log('endIdx', endIdx);
+  console.log('sliced', string.slice(startIdx, endIdx));
+  console.log(string.slice(startIdx, endIdx).trim() === methodStr.trim());
+  while (string.slice(startIdx, endIdx) !== methodStr.trim()) {
+    startIdx = string.indexOf(methodStr, endIdx);
+    endIdx = minBesidesNegOne(
+      string.indexOf('(', startIdx),
+      string.indexOf(' ', startIdx)
+    );
+  }
+  return startIdx;
+}
+
+function minBesidesNegOne(num1, num2) {
+  if (num1 === -1) {
+    return num2;
+  } else if (num2 === -1) {
+    return num1;
+  } else {
+    return Math.min(num1, num2);
   }
 }
 
@@ -78,4 +113,4 @@ module.exports = {
   getInsideOfFunc,
   validBraces,
   getClassName,
-}
+};
