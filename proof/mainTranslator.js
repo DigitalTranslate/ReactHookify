9289a45e7c81e52b7a58d364e3ae9951f6d8fda9
Cameron Ratliff <cratliff@colgate.edu>
2020-05-08 16:07:44 -0400

    working on class regex logic

--- proof/mainTranslator.js
+++ proof/mainTranslator.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-path-concat */
 
-const { str0, str1, str2, str3 } = require('./utils/exampleClass')
-const { handleConstructor } = require('./utils/constructorUtil')
-const fs = require('fs')
+const { str0, str1, str2, str3 } = require('./utils/exampleClass');
+const { handleConstructor } = require('./utils/constructorUtil');
+const fs = require('fs');
 const {
   hookifyPath,
   getBodyMethods,
@@ -10,77 +10,82 @@ const {
   getEndIdxOfFunc,
   getInsideOfFunc,
   getClassName,
-} = require('./utils/commonUtils')
-const { getBeforeReturn } = require('./utils/renderUtil')
-const { yellow } = require('chalk')
+  getClassCompIdx,
+} = require('./utils/commonUtils');
+const { getBeforeReturn } = require('./utils/renderUtil');
+const { yellow } = require('chalk');
 
 //THIS FUNCTION IS IN CHARGE OF TRANSLATING CLASS COMPONENT TO FUNCTIONAL COMPONENT && PUTS IT ALL TOGETHER
-function translateToFunctionComp(classCompInStr) {
-  const nameOfClass = getClassName(classCompInStr)
+function translateToFunctionComp(fileInString) {
+  const classCompIdx = getClassCompIdx(fileInString);
+  const beforeClass = fileInString.slice(0, classCompIdx);
+  const nameOfClass = getClassName(fileInString, classCompIdx);
   //checks whole class string from 'props'
-  const propsCheck = classCompInStr.includes('props') ? 'props' : ''
-  let startOfBodyIdx = classCompInStr.indexOf('{') + 1
+  const propsCheck = fileInString.includes('props') ? 'props' : '';
+  let startOfBodyIdx = fileInString.indexOf('{') + 1;
 
   //CONSTRUCTOR
-  const constructorCheck = /(constructor)[ ]*\(/.test(classCompInStr)
-  let handledConstructor = ''
+  const constructorCheck = /(constructor)[ ]*\(/.test(fileInString);
+  let handledConstructor = '';
 
   if (constructorCheck) {
-    handledConstructor = handleConstructor(classCompInStr)
-    startOfBodyIdx = getEndIdxOfFunc(classCompInStr, 'constructor')
+    handledConstructor = handleConstructor(fileInString);
+    startOfBodyIdx = getEndIdxOfFunc(fileInString, 'constructor');
   }
 
   //BODY
-  let body = getBody(classCompInStr, startOfBodyIdx)
+  let body = getBody(fileInString, startOfBodyIdx);
 
-  let funcs = []
-  getBodyMethods(funcs, body)
+  let funcs = [];
+  getBodyMethods(funcs, body);
 
-  const funcCheck = funcs.length ? true : false
+  const funcCheck = funcs.length ? true : false;
 
   //RETURN STATEMENT
-  const beforeReturn = getBeforeReturn(classCompInStr)
-  const returnSlice = getInsideOfFunc(classCompInStr, 'render')
+  const beforeReturn = getBeforeReturn(fileInString);
+  const returnSlice = getInsideOfFunc(fileInString, 'render');
 
   //FINAL TEMPLATE
-  let finalStr = `function ${nameOfClass}(${propsCheck}) {
+  let finalStr = `
+  ${beforeClass}
+  function ${nameOfClass}(${propsCheck}) {
     ${handledConstructor}
     ${funcCheck ? `\n${funcs.join('\n')}\n` : ''}
     ${beforeReturn}
     return (
       ${returnSlice}
     )
-  }`
-  return finalStr.replace(/this.props/gi, 'props')
+  }`;
+  return finalStr.replace(/this.props/gi, 'props');
 }
 
 //THIS FUNCTION TAKES CREATED STRING AND WRITES A FILE
 function createFunctionComponentFile(funcCompInStr, filepath) {
-  const newPath = hookifyPath(filepath)
+  const newPath = hookifyPath(filepath);
 
   fs.writeFile(newPath, funcCompInStr, (err, contents) => {
     //creates proof.js in proof directory
-    if (err) throw err
-    console.log(yellow('Made proof.js'))
-  })
+    if (err) throw err;
+    console.log(yellow('Made proof.js'));
+  });
 }
 
 //THIS FUNCTION TAKES FILEPATH, READS FILE AT PATH AND THEN CREATES
 //FUNCTIONAL COMPONENT EQUIVALENT
 async function readAndCreate(filepath) {
-  const content = await fs.promises.readFile(filepath, 'utf-8')
+  const content = await fs.promises.readFile(filepath, 'utf-8');
   //DO NOTE USE readFileSync
-  const funcComponent = translateToFunctionComp(content)
-  createFunctionComponentFile(funcComponent, filepath)
+  const funcComponent = translateToFunctionComp(content);
+  createFunctionComponentFile(funcComponent, filepath);
 }
 
 //TESTING WITH READING A FILE && CAN DO WITH CLI NOW
 // readAndCreate(__dirname + '/../client/app.js')
 
 //TESTING WITHOUT READING FILE
-const finalStr = translateToFunctionComp(str0) //change which string to test
+const finalStr = translateToFunctionComp(str0); //change which string to test
 // createFunctionComponentFile(finalStr, '/client/proof.js')
 
 module.exports = {
   readAndCreate,
-}
+};
--- proof/utils/commonUtils.js
+++ proof/utils/commonUtils.js
@@ -1,99 +1,120 @@
 /* eslint-disable no-lonely-if */
 function hookifyPath(pathStr) {
   /*   '/public/client/app.js'    */
-  let finalPath = pathStr
-  const slicingIdx = pathStr.lastIndexOf('.')
-  finalPath = pathStr.slice(0, slicingIdx)
-  const fileType = pathStr.slice(slicingIdx)
-  return `${finalPath}_Hookified${fileType}`
+  let finalPath = pathStr;
+  const slicingIdx = pathStr.lastIndexOf('.');
+  finalPath = pathStr.slice(0, slicingIdx);
+  const fileType = pathStr.slice(slicingIdx);
+  return `${finalPath}_Hookified${fileType}`;
   /*   '/public/client/app_HOOKIFIED.js'    */
 }
 
-function getClassName(string) {
-  const classIdx = string.indexOf('class')
-  const classSlice = string.slice(classIdx)
-  const nameOfClass = classSlice.split(/[ ]+/)[1]
-  return nameOfClass
+// returns the index of the word 'class' for the class Component
+function getClassCompIdx(string) {
+  return string.search(/(class)([ \t]+)([\S]+)([ \t]+)(extends)/);
 }
 
+// Returns the name of the class
+function getClassName(string, startIdx) {
+  const classSlice = string.slice(startIdx);
+  const nameOfClass = classSlice.split(/[ ]+/)[1];
+  return nameOfClass;
+}
+
+// Used to determine when a function has ended (aka the braces are valid)
+// This is used inside the next two functions
 function validBraces(braces) {
-  let matches = { '(': ')', '{': '}', '[': ']' }
-  let stack = []
-  let currentChar
+  let matches = { '(': ')', '{': '}', '[': ']' };
+  let stack = [];
+  let currentChar;
   for (let i = 0; i < braces.length; i++) {
-    currentChar = braces[i]
+    currentChar = braces[i];
     if ('(){}[]'.includes(currentChar)) {
       if (matches[currentChar]) {
         // opening braces
-        stack.push(currentChar)
+        stack.push(currentChar);
       } else {
         // closing braces
         if (currentChar !== matches[stack.pop()]) {
-          return false
+          return false;
         }
       }
     }
   }
-  return stack.length === 0 // any unclosed braces left?
+  return stack.length === 0; // any unclosed braces left?
 }
 
+// Returns everything between the starting and ending brackets of a function, object, or class
+// Example input: 'componentDidMount() { const x = 5 }'
+// Example output: 'const x = 5'
 function getInsideOfFunc(string, methodStr) {
-  let startIdx = string.indexOf(methodStr)
-  if (startIdx === -1) return
-  startIdx = string.indexOf('{', startIdx)
-  let endIdx = startIdx + 1
+  let startIdx = string.indexOf(methodStr);
+  if (startIdx === -1) return;
+  startIdx = string.indexOf('{', startIdx);
+  let endIdx = startIdx + 1;
 
-  let funcSlice = string.slice(startIdx, endIdx)
+  let funcSlice = string.slice(startIdx, endIdx);
   while (!validBraces(funcSlice)) {
-    endIdx++
-    funcSlice = string.slice(startIdx, endIdx)
+    endIdx++;
+    funcSlice = string.slice(startIdx, endIdx);
   }
   if (funcSlice.indexOf('return') > -1) {
     return funcSlice
       .slice(funcSlice.indexOf('return') + 6, funcSlice.length - 2)
-      .trim()
+      .trim();
   } else {
-    return funcSlice.slice(1, funcSlice.length - 2)
+    return funcSlice.slice(1, funcSlice.length - 2);
   }
 }
+
+// Very similar to the last function. Returns the index of when the function ends
+// Example input: 'componentDidMount() { const x = 5 } '
+// Example output: 35
 function getEndIdxOfFunc(string, methodStr) {
-  let startIdx = string.indexOf(methodStr)
-  if (startIdx === undefined) return
-  startIdx = string.indexOf('{', startIdx)
-  let endIdx = startIdx + 1
-  let funcSlice = string.slice(startIdx, endIdx)
+  let startIdx = string.indexOf(methodStr);
+  if (startIdx === undefined) return;
+  startIdx = string.indexOf('{', startIdx);
+  let endIdx = startIdx + 1;
+  let funcSlice = string.slice(startIdx, endIdx);
 
   while (!validBraces(funcSlice)) {
-    endIdx++
-    funcSlice = string.slice(startIdx, endIdx)
+    endIdx++;
+    funcSlice = string.slice(startIdx, endIdx);
   }
-  return endIdx
-}
-function getBody(string, startIdx) {
-  let endIdx = getEndIdxOfFunc(string, 'Component') - 1 // add in React.Component Logic
-  return string.slice(startIdx, endIdx).trim()
+  return endIdx;
 }
 
+// returns the body of the class component (aka everything after the constructor,
+// or everything after Component { if there is no constructor
+function getBody(string, startIdx) {
+  let endIdx = getEndIdxOfFunc(string, 'Component') - 1; // add in React.Component Logic
+  return string.slice(startIdx, endIdx).trim();
+}
+
+// Iterates through the body and adds all of the methods to an array called funcs.
+// The methods are put into array as strings
+// Example input: 'componentDidMount() { const x = 5 }  \n  method2() { const y = 6 }'
+// Example output: ['componentDidMount() { const x = 5 }', 'method2() { const y = 6 }']
 function getBodyMethods(funcs, bodyStr) {
   // find first non-white space (aka function starting index)
-  let nonWhiteSpaceIdx = bodyStr.search(/\S/)
+  let nonWhiteSpaceIdx = bodyStr.search(/\S/);
   if (nonWhiteSpaceIdx === -1) {
-    return
+    return;
   }
-  let newBody = bodyStr.substring(nonWhiteSpaceIdx)
-  let funcEndIdx = newBody.search(/[^a-zA-Z0-9_]/) // find function name ending index
-  let funcName = newBody.slice(0, funcEndIdx)
-  let inside = getInsideOfFunc(bodyStr, `${funcName}`)
-  let endOfFuncIdx = getEndIdxOfFunc(bodyStr, `${funcName}`)
-  newBody = bodyStr.substring(endOfFuncIdx)
+  let newBody = bodyStr.substring(nonWhiteSpaceIdx);
+  let funcEndIdx = newBody.search(/[^a-zA-Z0-9_]/); // find function name ending index
+  let funcName = newBody.slice(0, funcEndIdx);
+  let inside = getInsideOfFunc(bodyStr, `${funcName}`);
+  let endOfFuncIdx = getEndIdxOfFunc(bodyStr, `${funcName}`);
+  newBody = bodyStr.substring(endOfFuncIdx);
 
   let funcStringified = `function ${funcName}() {
       ${inside}
-    }`
+    }`;
   if (funcName.toLowerCase() !== 'render') {
-    funcs.push(funcStringified)
+    funcs.push(funcStringified);
   }
-  getBodyMethods(funcs, newBody)
+  getBodyMethods(funcs, newBody);
 }
 
 module.exports = {
@@ -104,4 +125,5 @@ module.exports = {
   getInsideOfFunc,
   validBraces,
   getClassName,
-}
+  getClassCompIdx,
+};
