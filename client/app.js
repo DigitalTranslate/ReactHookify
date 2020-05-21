0d7107d4b7edae20d879138e4672b84c90263850
Anderson Chan <andersonchan4574@gmail.com>
2020-05-21 12:58:08 -0400

    debugging

--- client/app.js
+++ client/app.js
@@ -1,31 +1,27 @@
 class FriendStatus extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = { isOnline: null };
-    this.handleStatusChange = this.handleStatusChange.bind(this);
+    super(props)
+    this.handleStatusChange = this.handleStatusChange.bind(this)
   }
 
-  async componentDidMount() {
-    await ChatAPI.subscribeToFriendStatus(this.props.friend.id);
+  componentDidMount() {
+    ChatAPI.subscribeToFriendStatus(this.props.friend.id)
   }
 
   componentWillUnmount() {
-    ChatAPI.unsubscribeFromFriendStatus(this.props.friend.id);
+    ChatAPI.unsubscribeFromFriendStatus(this.props.friend.id)
   }
-  handleStatusChange(status) {
+  handleStatusChange() {
     this.setState({
       isOnline: status.isOnline,
-    });
+    })
   }
 
   render() {
-    if (this.state.isOnline === null) {
-      return 'Loading...';
-    }
     return (
       <div>
         <div>this.state.isOnline ? 'Online' : 'Offline';</div>;
       </div>
-    );
+    )
   }
 }
