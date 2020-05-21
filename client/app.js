2966de8b6660567f39558a0fffc5c2616c95b212
Anderson Chan <andersonchan4574@gmail.com>
2020-05-21 13:31:23 -0400

    fixed

--- client/app.js
+++ client/app.js
@@ -1,27 +1,76 @@
-class FriendStatus extends React.Component {
+class DeviceList extends React.Component {
+  PAGELENGTH = 6
   constructor(props) {
     super(props)
-    this.handleStatusChange = this.handleStatusChange.bind(this)
+    this._renderRow = this._renderRow.bind(this)
+    this._paginateDevices = this._paginateDevices.bind(this)
   }
-
-  componentDidMount() {
-    ChatAPI.subscribeToFriendStatus(this.props.friend.id)
-  }
-
-  componentWillUnmount() {
-    ChatAPI.unsubscribeFromFriendStatus(this.props.friend.id)
-  }
-  handleStatusChange() {
-    this.setState({
-      isOnline: status.isOnline,
-    })
-  }
-
   render() {
+    const rowCount = this._paginateDevices().length
+    const rowHeight = 55
     return (
       <div>
-        <div>this.state.isOnline ? 'Online' : 'Offline';</div>;
+        <List
+          width={700}
+          height={Math.max(rowCount * rowHeight, 110)}
+          rowCount={rowCount}
+          rowRenderer={this._renderRow}
+          rowHeight={rowHeight}
+          id="device-list"
+          devices={this._paginateDevices()}
+        />
+        <div id="pagination">
+          <Button
+            size="sm"
+            type="button"
+            className="arrow-button"
+            onClick={() => this.props.changeCurrentPage(-1, false)}
+            disabled={this.props.currentPage <= 1}
+          >
+            <img src={BACK_ARROW_IMG} alt="Previous Page" />
+          </Button>
+          <p className="pagination-text">{this._getPaginationText()}</p>
+          <Button
+            size="sm"
+            type="button"
+            className="arrow-button"
+            onClick={() => this.props.changeCurrentPage(1, false)}
+            disabled={
+              Math.ceil(this.props.devices.length / this.PAGELENGTH) <=
+              this.props.currentPage
+            }
+          >
+            <img src={FRONT_ARROW_IMG} alt="Next Page" />
+          </Button>
+        </div>
       </div>
+    )
+  }
+  _getPaginationText = () => {
+    const curPage = this.props.currentPage
+    const totalPages = Math.max(
+      1,
+      Math.ceil(this.props.devices.length / this.PAGELENGTH)
+    )
+    return `${curPage} of ${totalPages}`
+  }
+  /**
+   * Gets the devices to be shown for the current page the user is on
+   */
+  _paginateDevices() {
+    return this.props.devices.slice(
+      (this.props.currentPage - 1) * this.PAGELENGTH,
+      this.props.currentPage * this.PAGELENGTH
+    )
+  }
+  _renderRow({ key, index, style }) {
+    return (
+      <DeviceRow
+        key={key}
+        style={style}
+        pickDevice={this.props.pickDevice}
+        deviceName={this._paginateDevices()[index]}
+      />
     )
   }
 }
--- client/app_Hookified.js
+++ client/app_Hookified.js
@@ -1,33 +1,63 @@
-import React, { useState, useEffect } from "react"
-function FriendStatus(props) {
-  const [isOnline, setIsOnline] = useState(null)
-
-  useEffect(() => {
-    function handleStatusChange(status) {
-      setIsOnline(status.isOnline)
-    }
-    ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange)
-    return () => {
-      ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange)
-    }
-  }, [])
-  useEffect(() => {
-    console.log("hi")
-    genericMethod(5)
-  }, [props.counter])
-
-  function genericMethod(num) {
-    handleStatusChange
-    return num + 1
+import React from "react"
+function DeviceList(props) {
+  function _getPaginationText() {
+    const curPage = props.currentPage
+    const totalPages = Math.max(1, Math.ceil(props.devices.length / PAGELENGTH))
+    return `${curPage} of ${totalPages}`
+  }
+  function _paginateDevices() {
+    return props.devices.slice(
+      (props.currentPage - 1) * PAGELENGTH,
+      props.currentPage * PAGELENGTH
+    )
+  }
+  function _renderRow({ key, index, style }) {
+    return (
+      <DeviceRow
+        key={key}
+        style={style}
+        pickDevice={props.pickDevice}
+        deviceName={_paginateDevices()[index]}
+      />
+    )
   }
 
-  if (isOnline === null) {
-    return "Loading..."
-  }
+  const rowCount = _paginateDevices().length
+  const rowHeight = 55
   return (
     <div>
-      {genericMethod(5)}
-      <div>isOnline ? 'Online' : 'Offline';</div>;
+      <List
+        width={700}
+        height={Math.max(rowCount * rowHeight, 110)}
+        rowCount={rowCount}
+        rowRenderer={_renderRow}
+        rowHeight={rowHeight}
+        id="device-list"
+        devices={_paginateDevices()}
+      />
+      <div id="pagination">
+        <Button
+          size="sm"
+          type="button"
+          className="arrow-button"
+          onClick={() => props.changeCurrentPage(-1, false)}
+          disabled={props.currentPage <= 1}
+        >
+          <img src={BACK_ARROW_IMG} alt="Previous Page" />
+        </Button>
+        <p className="pagination-text">{_getPaginationText()}</p>
+        <Button
+          size="sm"
+          type="button"
+          className="arrow-button"
+          onClick={() => props.changeCurrentPage(1, false)}
+          disabled={
+            Math.ceil(props.devices.length / PAGELENGTH) <= props.currentPage
+          }
+        >
+          <img src={FRONT_ARROW_IMG} alt="Next Page" />
+        </Button>
+      </div>
     </div>
   )
 }
